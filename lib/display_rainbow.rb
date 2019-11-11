78277171a180a48e11639f3c085de264261142fd
wondrous-operand-3019 <nicholas.wilson@nevada.unr.edu>
2019-11-11 20:28:06 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,10 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  message = ""
+  for x in colors
+    if x == (colors.length - 1)
+      break
+    end
+    message += "#{colors[x][0].uppercase}: #{colors[x]}, "
+  end
+  message += "#{colors[x][0].uppercase}: #{colors[x]}"
